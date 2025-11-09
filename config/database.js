c0fc3fa4a9eb779824b21ec835fd9d4b956926dc
Marc Chavarria <marc.a.chavarria3@gmail.com>
2025-11-08 19:38:52 -0500

    initial commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://marcachavarria3_db_user:kX9LQIcu0Vh5UeN1@cluster0.aenp44h.mongodb.net/demo-project?appName=Cluster0', 
+    'dbName': 'demo day'
+};
